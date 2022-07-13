d3d93a6a4fd82920686481c5293ab4f4903b3d18
svenandten <rickezxc@gmail.com>
2022-07-13 08:22:51 +0800

    updated schema and settings api

---
+++ pages/api/settings/delete_settings.ts
@@ -0,0 +1,63 @@
+import prisma from '../../../config/prisma';
+
+export default async (req, res) => {
+
+    if (req.method === "POST"){
+
+        const settingID = req.body.setting_id;
+        const configName = req.body.config_name;
+
+        try{
+            if(settingID){
+                const delete_setting = await prisma.settings.delete({
+                    where:{
+                        settingID : settingID
+                    }
+                })
+
+                res.status(200).json({
+                    "message" : `Deleted setting ${settingID}`,
+                    "result"  : delete_setting
+                });
+            } else if (configName){
+                const delete_setting = await prisma.settings.deleteMany({
+                    where:{
+                        configName : configName
+                    }
+                })
+
+                if (delete_setting.count > 0){
+                    res.status(200).json({
+                        "message" : `Deleted setting ${configName}`,
+                        "result"  : delete_setting
+                    });
+                } else {
+                    res.status(406).json({
+                        "message" : `Failed to delete setting ${configName}`,
+                        "result"  : delete_setting
+                    });
+                }
+
+            } else {
+                res.status(200).json({
+                    "message" : "Please specify either the setting_id OR config_name"
+                });
+            }
+
+        } catch (error) {
+            const code = error.code;
+            console.error(error.message)
+            // error code for record not found
+            if (code === 'P2025'){
+                res.status(406).json({"message":`Failed to delete setting; record not found`});
+            } else {
+                res.status(406).json({"message" : error.message});
+            }
+        }
+       
+    } else {
+        res.status(406).json({"message": `ERROR: ${req.method} method used; this endpoint only accepts POST methods`});
+    }
+    
+
+}

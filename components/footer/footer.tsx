511d963a38a024ec5182311f1ef50f291d2ab36a
harixcode <hari1000prasad@gmail.com>
2022-08-02 01:56:12 +0800

    added superuser logic to footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -2,9 +2,46 @@ import Link from "next/link";
 import styles from "./footer.module.css";
 import { useSession } from "next-auth/react";
 import SuperUserAuth from "../auth/SuperUserAuth";
+import { useEffect, useState } from "react";
 
 export default function Footer() {
   const { data: session, status } = useSession();
+  const [display, setDisplay] = useState<boolean>(false);
+
+  useEffect(() => {
+    validateUser(status);
+  }, []);
+
+  //superuser logic
+  const validateUser = async (status) => {
+    let userID: string;
+    if (status == "authenticated") {
+      userID = session.id.toString();
+    }
+
+    //console.log(userID);
+    const res = await fetch(`/api/superuser/get_all_superuser`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then(async (res) => {
+      const data = await res.json();
+      //const message = data.message;
+      const values = data.result;
+      for (var i = 0; i < values.length; i++) {
+        let user = values[i].userID.toString();
+        //console.log(user);
+        if (user == userID) {
+          console.log("superuser detected");
+          setDisplay(true);
+          break;
+        }
+      }
+      //console.log(userID);
+    });
+  };
 
   return (
     <footer className={styles.footer}>
@@ -13,7 +50,7 @@ export default function Footer() {
         <li className={styles.navItem}>
           <a href="https://github.com/algotuno/algotuno_web3">GitHub</a>
         </li>
-        {session ? (
+        {display ? (
           <li className={styles.navItem}>
             <Link href="/admin/main">
               <a>Admin Portal</a>

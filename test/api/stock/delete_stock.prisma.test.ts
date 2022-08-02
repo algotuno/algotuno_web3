a8ff04817cff1457a4e5c394cdb9eaecd6cbb559
svenandten <rickezxc@gmail.com>
2022-08-02 09:26:05 +0800

    updated stock testcases

--- test/api/stock/delete_stock.prisma.test.ts
+++ test/api/stock/delete_stock.prisma.test.ts
@@ -1,14 +1,10 @@
 import handler from "../../../pages/api/stock/delete_stock";
-import * as auth_check from '../../../config/auth_check';
 import prisma from "../../../config/prisma";
 import {createMocks} from "node-mocks-http";
-import fetchMock from 'jest-fetch-mock';
 
 describe("Test delete_stock.ts", () => {
 
-    fetchMock.enableMocks()
-
-    test("Delete Stock with valid ticker symbol and auth key expecting success", async () => {
+    test("Delete Stock with valid ticker symbol expecting success", async () => {
         // 1. mock the data
         const ticker_symbol = "TEST";
 
@@ -27,19 +23,6 @@ describe("Test delete_stock.ts", () => {
             "message" : `Deleted stock ${ticker_symbol}`,
             "result" : delete_stock_result
         }
-
-        // const authorization_check = jest.fn(()=>true);
-
-        // jest.mock('../../../config/auth_check.ts', () =>({
-        //     ...(jest.requireActual('../../../config/auth_check.ts')),
-        //     authorization_check:jest.fn()
-        // }))
-        
-        // authorization_check.mockReturnValue(true);
-
-        const ac = require('../../../config/auth_check')
-        // const spy = jest.spyOn(auth_check.authorization_check, )
-
         
         prisma.stock.findFirst = jest.fn().mockReturnValueOnce(findfirst_stock_result);
         prisma.stock.deleteMany = jest.fn().mockReturnValueOnce(delete_stock_result);
@@ -49,9 +32,6 @@ describe("Test delete_stock.ts", () => {
             method: 'POST',
             body: {
                 "ticker_symbol" : ticker_symbol
-            },
-            headers:{
-                'authorization': 'NEXT_PUBLIC_API_SECRET_KEY 9ddf045fa71e89c6d0d71302c0c5c97e'
             }
         });
 
@@ -63,6 +43,106 @@ describe("Test delete_stock.ts", () => {
         const res_output = JSON.parse(res._getData());
         console.log(res_output);
         expect(res_output).toEqual(app_result);
+    });
+
+    test("Delete Stock with invalid ticker symbol not in database expecting error", async () => {
+        // 1. mock the data
+        const ticker_symbol = "TEST";
+
+        const findfirst_stock_result = null;
+
+        const app_result = {
+            "message" : `Stock ${ticker_symbol} does not exist`
+        }
+        
+        prisma.stock.findFirst = jest.fn().mockReturnValueOnce(findfirst_stock_result);
+
+        // 2. input api call
+        const {req, res} = createMocks({
+            method: 'POST',
+            body: {
+                "ticker_symbol" : ticker_symbol
+            }
+        });
+
+        // 3. call the api
+        await handler(req, res);
+        expect(res._getStatusCode()).toBe(406);
+
+        // 4. verify its output
+        const res_output = JSON.parse(res._getData());
+        console.log(res_output);
+        expect(res_output).toEqual(app_result);
+    });
+
+    test("Delete Stock with valid ticker symbol expecting error", async () => {
+        // 1. mock the data
+        const ticker_symbol = "TEST";
+
+        const app_result = {
+            "message" : "mocked error"
+        }
+        
+        prisma.stock.findFirst = jest.fn().mockRejectedValueOnce(app_result);
+
+        // 2. input api call
+        const {req, res} = createMocks({
+            method: 'POST',
+            body: {
+                "ticker_symbol" : ticker_symbol
+            }
+        });
+
+        // 3. call the api
+        await handler(req, res);
+        expect(res._getStatusCode()).toBe(406);
+
+        // 4. verify its output
+        const res_output = JSON.parse(res._getData());
+        console.log(res_output);
+        expect(res_output).toEqual(app_result);
+    });
+
+    test("When omitting ticker_symbol in POST request expecting error", async () => {
+        // 1. mock the data
+        const app_result = {
+            "message" : "mocked error"
+        }
+        
+        prisma.stock.findFirst = jest.fn().mockRejectedValueOnce(app_result);
+
+        // 2. input api call
+        const {req, res} = createMocks({
+            method: 'POST'
+        });
+
+        // 3. call the api
+        await handler(req, res);
+        expect(res._getStatusCode()).toBe(406);
+
+        // 4. verify its output
+        const res_output = JSON.parse(res._getData());
+        console.log(res_output);
+        expect(res_output).toEqual(app_result);
+    });
+
+
+    test("When using GET instead of POST expecting error", async () => {
+        // 1. input api call
+        const {req, res} = createMocks({
+            method: 'GET'
+        });
+
+        // 2. call the api
+        await handler(req, res);
+        expect(res._getStatusCode()).toBe(406);
+
+        // 3. verify its output
+        const res_output = JSON.parse(res._getData());
+        console.log(res_output);
+        expect(res_output).toEqual({
+            "message" : `ERROR: ${req.method} method used; this endpoint only accepts POST methods`
+        });
     });
 
 });
